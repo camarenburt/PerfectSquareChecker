23edaa37f41acb77da236cbda4efac6390b02a2f
621352 <camarenburt>
2023-11-21 09:39:39 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int input;
+        boolean output = false;
+
+
+        System.out.println("Please enter a number");
+        input = scan.nextInt();
+        for(int i = 1; i <= input; i++){
+            if(i * i == input){
+                output = true;
+                break;
+            }
+        }
+        System.out.println("It is" + output);
     }
 }
